0af8bd8813ef34f566b950632ed73b2ce03131e0
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2022-08-23 21:06:21 -0300

    adicionando estilo para o titulo da cesta

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { Text, StyleSheet, Dimensions } from "react-native";
+import { Text, StyleSheet, Dimensions, Image } from "react-native";
 
 import topo from '../../assets/topo.png'
 
 const width = Dimensions.get('screen').width;
 
 export default function Cesta(){
-    return <Image source={topo} />
+    return  <>
+    <Image source={topo} style={estilos.topo} />
+    <Text style={estilos.titulo}>Detalhes da cesta</Text>
+    </>
 }
 
 const estilos = StyleSheet.create({
@@ -15,6 +18,16 @@ const estilos = StyleSheet.create({
         width: "100%",
         //altura da imagem divide pela largura da imagem e multiplica pelo width
         height: 578 / 768 * width,
+    },
+    titulo: {
+        position: "absolute",
+        width: "100%",
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight: 26,
+        color: "white",
+        fontWeight: "bold",
+        padding: 16
     }
 
 })
