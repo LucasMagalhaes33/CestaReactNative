9675518cc05d99214497aafe7520fc9d98369f72
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2022-08-23 22:43:21 -0300

    adicionando font montserrat ao projeto

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import { StatusBar, SafeAreaView, StyleSheet, View } from 'react-native';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold, Montserrat_400Regular_Italic } from '@expo-google-fonts/montserrat';
 
 import Cesta from './src/telas/Cesta';
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    "MontSerratRegular": Montserrat_400Regular,
+    "MontSerratBold": Montserrat_700Bold,
+  });
+
+  if (!fonteCarregada){
+    return <View></View>
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
